ee93a6978e5d3a3aceab666381a7bdcd06e67928
AnushaKamat <kamatanusha7@gmail.com>
2019-03-17 21:22:13 -0700

    Changed ACC class

--- README.md
+++ README.md
@@ -10,7 +10,8 @@ Implementation Details
 
 - Cruise Control would be inherited by a process class
 
-- Adaptive Cruise Control would be inherited either by Cruise Control/Process(Design choice)
+- Adaptive Cruise Control would be inherited either by Cruise Control/Process(Design choice).
+
     It will set Adaptive Cruise control speed depending on 
     - speed set by driver
     - another car in the safety distance
@@ -20,25 +21,72 @@ Implementation Details
     - set safety distance
     - press brake to override ACC
 
+Design Details(Added on 3/17):
+---
+- Cruise Control is being redesigned.
+    - Driver sets Cruise On/Off which is emitted as an event to CruiseControl class
+    - When CC is switched on , the driver also sets the desired speed which is sent over the channel.
+    - CruiseControl class has an handler for change in `CC_on`
+    - CruiseControl class sets the value of throttle accordingly and sends it over channel to the Car class.
+    - Car class, depending on the current velocity of the car and new throttle value, sets the new speed of the car.
+- Adaptive Cruise Control is designed in the class `AutoCruiseControl`.
+    - Driver switches on `ACC_on` switch to enable Adaptive Cruise Control to kick in.This triggers an event.
+    - When ACC is switched on, Driver has to set desired speed and safety distance.
+    - In AutoCruiseControl class, depending on `ACC status` event, the desired speed, safety distance and current velocity of Car is read in.
+    - In addition to this, ACC is being catered to by another class called Radar inherited from Process.
+        Radar sends the distance from the car to the next obstacle.
+    - In ACC, if obstacle is farther than safety distance, the ACC keeps cruising at the same desired speed set by the driver.
+    - If ACC finds that obstacle is in the range of safety distance set, in order to prevent damage, it decreases the desired speed by 5 till it     can maintain the safe distance from the obstacle. <br/>
+
+    Assumptions: <br/>
+    The obstacle is also contsantly moving wrt to car either at slow speed or higher speed than that of ACC car.
+
+
+
 Milestones 
 ---
 1. Talk to Professor about the project and it's feasibility. Date : 03/09 -DONE
-2. Create ACC class and share events between driver, car and ACC. Date : 03/13
-3. Create driving environment where another interacts with car enabled with ACC. Date : 03/17
+2. Create ACC class and share events between driver, car and ACC. Date : 03/13<br/>
+    PROGRESS :<br/>
+        - 3/15 : Cruise Control is modified/ Simulated Time run is created<br/>
+        - 3/16 : Different Classes for CC and ACC. Driver Class is modified to accomodate different driving styles.
+3. Create driving environment where another interacts with car enabled with ACC. Date : 03/17<br/>
+    PROGRESS : <br/>
+        - 3/17 : Tested the driving scenarios using the environment.<br/>
+
+    TODO : <br/>
+        - Modularise ACC example with different classes in different files.<br/>
+        - Adapt to ELMA project Folder structure. <br/>
+        - Check the new pull for Simulated Run and compare with existing one. <br/>
+
 4. Write the test cases and get the testing done. Date : 03/20
 5. Submit documentation with tests. Date : 03/22
 
 Strech Goals
 ---
-1. Interaction of car enabled with ACC with a regular car
+1. Interaction of car enabled with ACC with a regular car.<br/>
+    PROGRESS: Interaction of ACC enabled car with moving obstacle is met. Obstacle is not modelled as a Car.
 2. Car with Camera function and not ACC. - which could read sign boards and change the speed of car dynamically till driver intervenes.
 3. Simulation of Car with respect to ACC enabled Car.
 4. Plot of distance covered by regular car vs the distance covered by ACC enabled car wrt time.(depicting its speed curve)
+5. [3/17] Consider having a UI using ncurses.
 
 Installation
 ---
 
 The source code for Elma [is on Github](https://github.com/klavinslab/elma).
+Also forked in this project.
+
+From Dockerhub
+---
+
+To get started, you will need a C++ build environment. The Docker container `elma` is available on Dockerhub, which can be used as follows:
+
+    git clone https://github.com/AnushaKamat/AdaptiveCruiseControl.git
+    cd AdaptiveCruiseControl
+    docker run -v $PWD:/source -it klavins/elma:latest bash
+    make
+    ./examples/bin/ACC
 
 
 
--- examples/ACC.cc
+++ examples/ACC.cc
@@ -11,7 +11,7 @@ using namespace elma;
 namespace driving_example {
 
     //! Example: Another car simulation process. See examples/driving.cc.
-    typedef enum { REGULAR, CC, ACC } car_type;
+    typedef enum { REGULAR, CC , ACC} car_type;
     const double KP = 314.15;
     //! See the file examples/driving.cc for usage.
     class Car : public Process {
@@ -55,7 +55,7 @@ namespace driving_example {
         const double k = 0.02;
         const double m = 1000;
         int status; // 0 - regular mode, 1 - CC 
-        car_type model;
+        
     };  
     
     //! Example: A cruise controller for a Car process.  See examples/driving.cc.
@@ -103,6 +103,118 @@ namespace driving_example {
         int CC_status;    
     };
 
+    //! Example: A cruise controller for a Car process.  See examples/driving.cc.
+
+    //! See the file examples/driving.cc for usage.
+    class AutoCruiseControl : public Process {
+
+        public:
+
+        //! Wrap the base process class
+        //! \param name The name of the controller      
+        AutoCruiseControl(std::string name) : Process(name) {}
+
+        //! Watch for events that change the desired speed.
+        void init() {
+            watch("ACC status", [this](Event& e) {
+                ACC_status = e.value();
+            });
+            watch("Obstacle",[this](Event& e){
+                obs_dist = e.value();
+            });
+        }
+
+        //! Nothing to do to initialize    
+        void start() {}
+
+        //! Get the velocity from the Velocity Channel, compute
+        //! a simple proportional control law, and send the result
+        //! to the Throttle channel.    
+        void update() {
+            if(ACC_status){
+                if(channel("SafetyDistance").nonempty()){
+                    sfdist = channel("SafetyDistance").latest();
+                }
+                
+                if ( channel("Velocity").nonempty() ) {
+                        speed = channel("Velocity").latest();
+                }
+                
+                if(obs_dist > sfdist){
+                    
+                    if(channel("DesSpeed").nonempty()){
+                        desired_speed = channel("DesSpeed").latest();
+                        
+                    }
+                    channel("Throttle").send(-KP*(speed - desired_speed));
+                    
+                }
+                else{
+                    if(obs_dist < sfdist){
+                        desired_speed -=5;
+                        channel("Throttle").send(-KP*(speed - desired_speed));
+                        
+                    }
+                }
+            }
+        }
+
+        //! Nothing to do to stop    
+        void stop() {}
+
+        private:
+        double speed = 0;
+        double desired_speed = 0.0;
+        int ACC_status;  
+        int obs_dist; 
+        int sfdist; 
+    };
+
+
+    class Radar : public Process {
+        public:
+
+        //! Wrap the base process class
+        //! \param name The name of the car    
+        Radar(std::string name) : Process(name) {}
+
+        //Car(std::string name,car_type model) : Process(name) {}
+            
+        //! Nothing to do to initialize
+        void init() {
+            obs_dist = 10;
+        }
+
+        //! To start a new simulation, this process sets
+        //! the car's velocity to zero kph.    
+        void start() {}
+
+        //! The update method gets the latest force from the 
+        //! Throttle Channel, if any. Then it updates the 
+        //! car's velocity, and sends it out on the Velocity
+        //! Channel.     
+        void update() {
+            if(obs_dist == 10){
+                obs_dist = 3;
+            }
+            else {                           
+                obs_dist = 10;
+            }
+            emit(Event("Obstacle",obs_dist));
+            //std::cout << "3. Obstacle distance set to : " << obs_dist <<"\n";
+            
+        }
+
+        //! Nothing to do to stop    
+        void stop() {}
+
+        private:
+        int obs_dist;
+        
+    };  
+ 
+
+
     //! Example: A simulated driver, who keeps cycling between 50 and 60 kph.  See examples/driving.cc.
     class Driver : public Process {
 
@@ -115,6 +227,7 @@ namespace driving_example {
         //! initialize the desired speed
         void init() {
             desired_speed = 50;
+            sfdist = 5;
         }
 
         //! Nothing to do to start
@@ -123,34 +236,80 @@ namespace driving_example {
         //! If the desired speed is 50, change to 60,
         //! otherwise change to 50.
         void update() {
-            std::cout<<"CC status : " <<CC_on <<std::endl;
-            if(CC_on){
-                emit(Event("CC status",CC_on));
-                if(desired_speed == 50){
-                    desired_speed = 60;             //Driver must emit CC_on event - then car operates with CC mode or else bypasses directly to accped mode
-                } 
-                else {                            //Driver emits CC_off or -ve throttle >> CC mode off
-                    desired_speed = 50;
-                }
-                CC_on =0;
-                channel("DesSpeed").send(desired_speed);
+            std::cout<<"operation mode : " <<operation_mode <<std::endl;
+            if(CC_on == 0 && ACC_on == 0){
+                operation_mode = REGULAR;
+                
+            }
+            
+            switch (operation_mode){
+                case ACC :      ACC_on = 1;
+                                emit(Event("ACC status",ACC_on));
+                                if(desired_speed == 50){
+                                    desired_speed = 70;             //Driver must emit CC_on event - then car operates with CC mode or else bypasses directly to accped mode
+                                } 
+                                else {                            //Driver emits CC_off or -ve throttle >> CC mode off
+                                    desired_speed = 50;
+                                }
+                                
+                                sfdist = 4;
 
+                                std::cout <<"1.Desired Speed :  " << desired_speed <<std::endl;
+                                std::cout <<"2.Safety Distance :  "<<sfdist << std::endl;
+                                channel("SafetyDistance").send(sfdist);
+                                channel("DesSpeed").send(desired_speed);
+                                operation_mode = REGULAR;
+                                break;
+                case CC :           CC_on = 1;
+                                    emit(Event("CC status",CC_on));
+                                    if(desired_speed == 50){
+                                        desired_speed = 60;             //Driver must emit CC_on event - then car operates with CC mode or else bypasses directly to accped mode
+                                    } 
+                                    else {                            //Driver emits CC_off or -ve throttle >> CC mode off
+                                        desired_speed = 50;
+                                    }
+                                    
+                                    channel("DesSpeed").send(desired_speed);
+                                    operation_mode = REGULAR;
+
+                                    
+                                    
+                                break;
+
+                case REGULAR:   
+                                
+                                accped =45;
+                                std::cout << "In regular accped mode";
+                                
+                                if(ACC_on ==1){
+                                    operation_mode = ACC;
+                                }
+                                else
+                                 if (CC_on == 1){
+                                    operation_mode = CC;
+                                }
+                                
+
+                                channel("Throttle").send(KP*accped);
+                                break;
             }
-            else{
-                accped =5;
-                emit(Event("CC status",CC_on));
-                CC_on= 1;
-                channel("Throttle").send(-KP*accped);
-            }
+            
         }
-
+        
         //! Nothing to do to stop
         void stop() {}
 
         private:
         double desired_speed;
         double accped;
+        // For CC :
+        // CC_on =1 and operation_mode = CC
+        // For ACC :
+        // ACC_on =1 and operation_mode = ACC
         bool CC_on = 0;
+        bool ACC_on = 1;
+        car_type operation_mode = ACC;
+        int sfdist;
     };
 }
 
@@ -161,6 +320,8 @@ int main() {
     driving_example::Car acar("CarA"); // can specify type of car in constructor, Regular,CC,ACC,Camera
     driving_example::CruiseControl cc("Control");
     driving_example::Driver driver("Steve");
+    driving_example::AutoCruiseControl acc("AutoControl");
+    driving_example::Radar sens("RadarSensor");
     Channel throttle("Throttle");
     Channel velocity("Velocity");
     //Channel ccstatus("CC status");
@@ -170,6 +331,8 @@ int main() {
 
     m.schedule(acar, 100_ms)
     .schedule(cc, 100_ms)
+    .schedule(acc,100_ms)
+    .schedule(sens,100_ms)
     .schedule(driver, 5_s)
     .add_channel(throttle)
     .add_channel(velocity)
